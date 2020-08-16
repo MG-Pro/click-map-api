6799bf6d0f70b9e453066761f7cbaaa142901859
MG <mg84@bk.ru>
2020-08-17 00:23:06 +0300

    add user create date, improve orientationModel,

--- models/AbstractModel.js
+++ models/AbstractModel.js
@@ -16,12 +16,6 @@ class AbstractModel {
     const result = await this.query('SELECT LAST_INSERT_ID()')
     return result[0]['LAST_INSERT_ID()']
   }
-  
-  normalizeActivityValues(values) {
-    return Object.values(values)
-      .map((item) => (typeof item === 'string' ? `'${item}'` : Math.round(item)))
-      .join(', ')
-  }
 }
 
 export default AbstractModel
