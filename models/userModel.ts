5360c9e323b1cbffb30f6e3907740ca832704cb2
MG <mg84@bk.ru>
2020-08-20 23:12:52 +0300

    adding ts in process

--- models/userModel.ts
+++ models/userModel.ts
@@ -2,19 +2,19 @@ import db from '../db/index.js'
 import AbstractModel from './AbstractModel.js'
 
 class UserModel extends AbstractModel {
-  async getByFingerprint(fingerprint) {
+  public async getByFingerprint(fingerprint) {
     const sqlSelect = `SELECT * FROM users WHERE fingerprint="${fingerprint}"`
     const result = await this.query(sqlSelect)
     return result[0]
   }
 
-  async add(fingerprint) {
+  public async add(fingerprint) {
     const sqlInsert = `INSERT INTO users(fingerprint) VALUES ("${fingerprint}")`
     await this.query(sqlInsert)
     return this.getLastId()
   }
 
-  async getList() {
+  public async getList() {
     const sqlSelect = `
         SELECT u.id, COUNT(a.user_id), u.create_date as activities
         FROM users u
